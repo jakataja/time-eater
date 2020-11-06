d3f05324878e2c20ccd0cace6d1284d0fa24f3c0
Katarzyna Bak <katarzyna_bak@epam.com>
2020-11-06 20:24:01 +0100

    remove commented lines from App.vue

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -8,16 +8,13 @@
               type="button">{{ tab }}</button>
     </div>
     <div class="time-eater-tab-content">
-<!--      <p>Hostname: {{currTab}}</p>-->
-<!--      <p class="clock">{{displayedCurrTime}}</p>-->
-<!--      <p>Total: {{displayedTotalTime}}</p>-->
       <component v-bind:is="currTabComponent"></component>
     </div>
   </div>
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex';
+
 import Current from './Current';
 import Today from './Today';
 import Week from './Week';
@@ -25,7 +22,6 @@ import Week from './Week';
 export default {
   data() {
     return {
-      // timer: null,
       currTabComponent: 'current',
       tabComponents: ['current', 'today', 'week'],
     };
@@ -34,54 +30,6 @@ export default {
     current: Current,
     today: Today,
     week: Week,
-  },
-  // computed: {
-  //   displayedCurrTime() {
-  //     const currTimeObj = this.createTimeObject(this.currTabTime);
-  //     return this.formatTime(currTimeObj);
-  //   },
-  //   displayedTotalTime() {
-  //     const totalTimeObj = this.createTimeObject(this.currTabTimeTotal);
-  //     return this.formatTime(totalTimeObj);
-  //   },
-  //   ...mapState({
-  //     currTab: (state) => state.currTab,
-  //     currTabTime: (state) => state.currTabTime,
-  //   }),
-  //   ...mapGetters([
-  //     'currTabTimeTotal',
-  //   ]),
-  // },
-  // methods: {
-  //   createTimeObject(time) {
-  //     const hours = Math.floor(time / 3600);
-  //     const leftTime = time - hours * 3600;
-  //     const minutes = Math.floor(leftTime / 60);
-  //     const seconds = leftTime - minutes * 60;
-  //
-  //     return { h: hours, m: minutes, s: seconds };
-  //   },
-  //   formatTime(timeObj) {
-  //     const { h, m, s } = timeObj;
-  //     return `${this.z(h)}:${this.z(m)}:${this.z(s)}`;
-  //   },
-  //   z(number) {
-  //     // put zero before
-  //     return ((number > 9) ? `${number}` : `0${number}`);
-  //   },
-  //   getAll() {
-  //     const tabs = [];
-  //     // eslint-disable-next-line no-plusplus
-  //     for (let i = 0; i < localStorage.length; i++) {
-  //       tabs[i] = {
-  //         url: localStorage.key(i),
-  //         time: localStorage.getItem(localStorage.key(i)),
-  //       };
-  //     }
-  //     this.$store.commit('SET_ALL_TABS', tabs);
-  //   },
-  // },
-  mounted() {
   },
 };
 </script>
