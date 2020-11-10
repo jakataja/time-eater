a691a4071a0289e692f495a583f773cf7f380ed0
Katarzyna Bak <katarzyna_bak@epam.com>
2020-11-10 13:33:12 +0100

    mock date before each test

--- src/store/getters.test.js
+++ src/store/getters.test.js
@@ -3,20 +3,11 @@ import * as getters from './getters';
 const mockDate = new Date('2020-11-03T10:20:30Z');
 const RealDate = Date;
 
-// beforeAll(() => {
-//   global.Date = jest.fn(() => mockDate);
-// });
-//
-// afterAll(() => {
-//   global.Date = RealDate;
-// });
-
-
 describe('getters', () => {
   let state;
 
   beforeEach(() => {
-    // global.Date = jest.fn(() => mockDate);
+    global.Date = jest.fn(() => mockDate);
     state = {
       foo: 'bar',
       currTab: 'www.google.com',
@@ -33,7 +24,7 @@ describe('getters', () => {
   });
 
   afterEach(() => {
-    // global.Date = RealDate;
+    global.Date = RealDate;
   });
 
   it('"currTabTimeTotal" returns time spent in active Tab that was visited before', () => {
@@ -50,24 +41,19 @@ describe('getters', () => {
   });
 
   it('"getTodayAll" returns all visited tabs today', () => {
-    global.Date = jest.fn(() => mockDate);
     expect(getters.getTodayAll(state)).toMatchObject([
       { url: '2020-11-03_www.google.com', time: '98129' },
       { url: '2020-11-03_www.github.com', time: '45732' },
     ]);
-    global.Date = RealDate;
   });
 
   it('"sumToday" returns parsed total time spent in all tabs today', () => {
-    global.Date = jest.fn(() => mockDate);
     const getTodayAll = [];
     getTodayAll.reduce = jest.fn(() => 143861);
     expect(getters.sumToday(state, { getTodayAll })).toContain('39:57:41');
-    global.Date = RealDate;
   });
 
   it('"getTodayAllParsed" returns all visited tabs today', () => {
-    global.Date = jest.fn(() => mockDate);
     const getTodayAll = [
       { url: 'www.google.com', time: { h: '27', m: '15', s: '29' } },
       { url: 'www.github.com', time: { h: '12', m: '42', s: '12' } },
@@ -80,29 +66,23 @@ describe('getters', () => {
       { url: 'www.google.com', time: '27:15:29' },
       { url: 'www.github.com', time: '12:42:12' },
     ]);
-    global.Date = RealDate;
   });
 
   it('"get7DaysAll " returns returns all visited tabs for last 7 days', () => {
-    global.Date = jest.fn(() => mockDate);
     expect(getters.get7DaysAll(state)).toMatchObject([
       { url: '2020-11-03_www.google.com', time: '98129' },
       { url: '2020-11-03_www.github.com', time: '45732' },
       { url: '2020-10-28_www.gmail.com', time: '232' },
     ]);
-    global.Date = RealDate;
   });
 
   it('"sum7Days" returns parsed total time spent in all tabs for last 7 days', () => {
-    global.Date = jest.fn(() => mockDate);
     const get7DaysAll = [];
     get7DaysAll.reduce = jest.fn(() => 144093);
     expect(getters.sum7Days(state, { get7DaysAll })).toContain('40:01:33');
-    global.Date = RealDate;
   });
 
   it('"get7DaysAllParsed " returns returns all visited tabs for last 7 days', () => {
-    global.Date = jest.fn(() => mockDate);
     const get7DaysAll = [
       { url: '2020-11-03_www.google.com', time: '98129' },
       { url: '2020-11-03_www.github.com', time: '45732' },
@@ -118,6 +98,5 @@ describe('getters', () => {
       { url: 'www.github.com', time: '12:42:12' },
       { url: 'www.gmail.com', time: '00:03:52' },
     ]);
-    global.Date = RealDate;
   });
 });
